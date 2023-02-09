9c27f7b20a2fce727353df0269f81e44809c1ef9
dev1 <dev1@gmail.com>
2023-02-09 09:56:47 +0530

    cleaning the workspace

--- README.md
+++ README.md
@@ -1,4 +1,2 @@
 # batch44
 this repo is for b44 and b42
-
-This is new line from Github remote repo
--- development.java
+++
@@ -1,3 +0,0 @@
-This is updation from dev branch
-This is another update from dev
-This is issue fix
